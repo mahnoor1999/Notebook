0c0377ea5b7db51226e1d3052c1c48eb8c8e7cd5
nimra555 <rosegulab505@gmail.com>
2020-11-14 09:05:32 +0500

    add Content  in add page

--- src/Components/Body/Navbar/AddBtn/AddBtn.js
+++ src/Components/Body/Navbar/AddBtn/AddBtn.js
@@ -1,7 +1,23 @@
-import React from 'react'
-const AddBtn  = () => {
-    return(
-        <h1>AddBtn</h1>
+import React,{useState} from 'react';
+import RadioBtn from './RadioButton/RadioButton';
+import InputValue from './InputValue/InputValue';
+import InputData  from './InputData/InputData';
+
+const AddBtn = () => {
+    const [inputHead,setinputHead] = useState("");
+    const [inputContent,setinputContent] = useState("");
+    const [addInput,setaddInput] = useState([]);
+
+    return (
+        <div>
+
+            
+            <RadioBtn />
+            <br></br>
+            <InputValue setinputHead={setinputHead} inputHead={inputHead}/>
+            <InputData setinputContent={setinputContent} inputContent={inputContent}/>
+            <button onClick={inputHead}>Add</button>
+        </div>
     )
 }
 export default AddBtn
