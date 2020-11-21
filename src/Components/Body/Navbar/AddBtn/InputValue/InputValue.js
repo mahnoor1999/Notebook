ed00fe4ff2b3398c259262960e6f885940b40556
nimra555 <rosegulab505@gmail.com>
2020-11-21 23:13:49 +0500

    add react-firebase and firebase pkges

--- src/Components/Body/Navbar/AddBtn/InputValue/InputValue.js
+++ src/Components/Body/Navbar/AddBtn/InputValue/InputValue.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import firebase from '../../firebase';
 
 const InputValue = ({setInputHead}) => {
 
+    const changeHandle=()=>{
+        const inputRef = firebase.database().ref("noteboik");
+        const input = {
+            setInputHead,
+            complete: false,
+        }
+        inputRef.push(input);
+        setInputHead(e.target.value)
+    }    
     return(
         <div>
-             <input type="text" placeholder="Add heading" onChange={(e) => setInputHead(e.target.value)} />
+             <input type="text" placeholder="Add heading" onChange={changeHandle} />
         </div>
     )
 }
