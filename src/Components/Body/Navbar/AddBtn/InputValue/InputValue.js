c160b54a4b7ac3001a68552b90a516fbb4420d9d
Mahnoor Naveed <mahnoornaveed74@gmail.com>
2020-11-24 19:39:13 +0500

    Revert "add react-firebase and firebase pkges"

--- src/Components/Body/Navbar/AddBtn/InputValue/InputValue.js
+++ src/Components/Body/Navbar/AddBtn/InputValue/InputValue.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import firebase from '../../firebase';
 
 const InputValue = ({setInputHead}) => {
 
-    const changeHandle=()=>{
-        const inputRef = firebase.database().ref("noteboik");
-        const input = {
-            setInputHead,
-            complete: false,
-        }
-        inputRef.push(input);
-        setInputHead(e.target.value)
-    }    
     return(
         <div>
-             <input type="text" placeholder="Add heading" onChange={changeHandle} />
+             <input type="text" placeholder="Add heading" onChange={(e) => setInputHead(e.target.value)} />
         </div>
     )
 }
